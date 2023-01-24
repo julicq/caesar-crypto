e720bee202eb620329c02b727e436b9ddb2d07f4
julicq <julicq@gmail.com>
2023-01-24 11:31:19 +0300

    fix length of possible message to 128bytes

--- hash_encryption.py
+++ hash_encryption.py
@@ -33,7 +33,7 @@ def decrypt_data(data, private_key):
 
 def main():
     # Generate a new public-private key pair
-    (public_key, private_key) = rsa.newkeys(512)
+    (public_key, private_key) = rsa.newkeys(1024)
 
     # Input data
     data = input("Enter a message to encrypt: ")
